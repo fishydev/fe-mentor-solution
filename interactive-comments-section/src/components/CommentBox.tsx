0aae2823749f9b752baeaa3d7a695328beb7edb2
fishydev <huvuhuvuhuvu@gmail.com>
2023-04-11 13:24:56 +0700

    new comment animation

--- interactive-comments-section/src/components/CommentBox.tsx
+++ interactive-comments-section/src/components/CommentBox.tsx
@@ -11,6 +11,7 @@ import { ParentContext } from "../context/ParentContext"
 import Overlay from "./Overlay"
 import DeleteCommentModal from "./DeleteCommentModal"
 import CommentFoot from "./CommentFoot"
+import { Transition } from "@headlessui/react"
 
 type CommentBoxProps = Comment
 
@@ -63,32 +64,43 @@ const CommentBox = (props: CommentBoxProps) => {
 
   return (
     <CommentContext.Provider value={props}>
-      <div className="CommentBox">
-        <div className="flex p-4 bg-white gap-4 rounded-xl">
-          <VoteButton className="hidden md:flex py-2" />
-          <div className="flex flex-col gap-2 flex-grow">
-            <CommentHead
-              onClickDelete={handleClickDelete}
-              onClickReply={handleClickReply}
-              onClickEdit={handleClickEdit}
-            />
-            <CommentBody />
-            <CommentFoot
-              onClickDelete={handleClickDelete}
-              onClickReply={handleClickReply}
-              onClickEdit={handleClickEdit}
-            />
+      <Transition
+        appear={true}
+        show={true}
+        enter="transition-all ease-in-out duration-500 delay-[200ms]"
+        enterFrom="opacity-0 translate-y-6"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition-all ease-in-out duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="CommentBox">
+          <div className="flex p-4 bg-white gap-4 rounded-xl">
+            <VoteButton className="hidden md:flex py-2" />
+            <div className="flex flex-col gap-2 flex-grow">
+              <CommentHead
+                onClickDelete={handleClickDelete}
+                onClickReply={handleClickReply}
+                onClickEdit={handleClickEdit}
+              />
+              <CommentBody />
+              <CommentFoot
+                onClickDelete={handleClickDelete}
+                onClickReply={handleClickReply}
+                onClickEdit={handleClickEdit}
+              />
+            </div>
           </div>
+          {inputType && (
+            <CommentInput
+              className="mt-2"
+              type={inputType}
+              onSubmit={handleSubmit}
+              placeholder={inputPlaceholder}
+            />
+          )}
         </div>
-        {inputType && (
-          <CommentInput
-            className="mt-2"
-            type={inputType}
-            onSubmit={handleSubmit}
-            placeholder={inputPlaceholder}
-          />
-        )}
-      </div>
+      </Transition>
       {showDelete && (
         <Overlay>
           <DeleteCommentModal
